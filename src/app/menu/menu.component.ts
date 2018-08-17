1b2e82e322c97c8b8ebda50df8f43686dbaabcad
vafin94 <vafinalex@gmail.com>
2018-08-17 17:35:37 +0300

    active menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,36 +3,38 @@ import {Menu} from './menu';
 import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
 
-  menuList: Menu[] = [
-    {link: 'aboutCompany', name: 'О компании' , ukrname: 'Про компанію'},
-    {link: 'osmdCreation', name: 'Создание ОСМД' , ukrname: 'Створення ОСМД'},
-    {link: 'ourServices', name: 'Услуги' , ukrname: 'Послуги'},
-    {link: 'news', name: 'Статьи и объяления' , ukrname: 'Статті та оголошення'},
-    {link: 'vacancies', name: 'Вакансии' , ukrname: 'Вакансії'},
-    {link: 'contacts', name: 'Контакты' , ukrname: 'Контакти'},
-    {link: 'gallery', name: 'Галерея' , ukrname: 'Галерея'},
-    {link: 'login', name: 'Личный кабинет' , ukrname: 'Особистий кабінет'},
-  ];
-  active = false;
-  currentLang = 'ru';
+    menuList: Menu[] = [
+        {link: '/aboutCompany', name: 'О компании', ukrname: 'Про компанію'},
+        {link: '/osmdCreation', name: 'Создание ОСМД', ukrname: 'Створення ОСМД'},
+        {link: '/ourServices', name: 'Услуги', ukrname: 'Послуги'},
+        {link: '/news', name: 'Статьи и объяления', ukrname: 'Статті та оголошення'},
+        {link: '/vacancies', name: 'Вакансии', ukrname: 'Вакансії'},
+        {link: '/contacts', name: 'Контакты', ukrname: 'Контакти'},
+        {link: '/gallery', name: 'Галерея', ukrname: 'Галерея'},
+        {link: '/login', name: 'Личный кабинет', ukrname: 'Особистий кабінет'},
+    ];
+    address = '';
+    currentLang = 'ru';
     constructor(private translate: TranslateService) {
         translate.setDefaultLang('ru');
     }
 
-  ngOnInit() {
-  }
-  isActive() {
-    this.active = true;
-  }
+    ngOnInit() {
+        this.getURL();
+    }
+
     useLanguage(language: string) {
         this.translate.use(language);
         this.currentLang = language;
     }
 
+    getURL() {
+        this.address = window.location.pathname;
+    }
 }
