c916e427453e7593c50e23cf9251a91bcf08d0bd
vafin94 <vafinalex@gmail.com>
2018-08-27 13:23:10 +0300

    Mail Service

--- src/app/contacts/message/message/message.component.ts
+++ src/app/contacts/message/message/message.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+
 
 @Component({
-  selector: 'app-message',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+    selector: 'app-message',
+    templateUrl: './message.component.html',
+    styleUrls: ['./message.component.css']
 })
 export class MessageComponent implements OnInit {
+    constructor(private http: HttpClient) {
+    }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    OnSubmit(mailForm: NgForm) {
+        console.log(JSON.stringify(mailForm.value));
+        this.http.post('https://xoz.od.ua/send_mailto.php', JSON.stringify(mailForm.value));
+    }
 }
